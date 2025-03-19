4ba42225b2faa1b6b49f4ced07e9802e68bd207e
SpringSunYY <3217778348@qq.com>
2025-03-19 16:04:13 +0800

    引入测试、hutool

---
+++ order-admin/src/test/java/com/lz/test/CommonTest.java
@@ -0,0 +1,20 @@
+package com.lz.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Project: order
+ * Package: com.lz.test
+ * Author: YY
+ * CreateTime: 2025-03-19  15:58
+ * Description: CommonTest
+ * Version: 1.0
+ */
+@SpringBootTest
+public class CommonTest {
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
