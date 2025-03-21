b428ee767433267cd7034fe1c9030ff2d4c07a71
SpringSunYY <3217778348@qq.com>
2025-03-21 11:00:53 +0800

    测试获取店铺

--- order-admin/src/test/java/com/lz/test/CommonTest.java
+++ order-admin/src/test/java/com/lz/test/CommonTest.java
@@ -29,7 +29,7 @@ public class CommonTest {
 
     private static final String CLIENT_ID = "367971";
     private static final String CLIENT_SECRET = "df2bac03-1006-4974-89f1-8163162d4910";
-    private static final String ACCESS_TOKEN = "1744c230-dbde-4fdf-99a5-fa0f2deb5de9";
+    private static final String ACCESS_TOKEN = "131db684-96a3-4b97-8d7e-85ca281b72c7";
 
     @Test
     public void test() {
@@ -55,7 +55,7 @@ public class CommonTest {
         String clientSecret = "df2bac03-1006-4974-89f1-8163162d4910";
         String grantType = "client_credentials";
         String url = StringUtils.format("https://openapi.sellfox.com/api/oauth/v2/token.json?client_id={}&client_secret={}&grant_type={}", clientId, clientSecret, grantType);
-        System.out.println("url = " + url);
+        System.out.println("curl -X GET \"" + url+"\"");
         //发送请求
         HttpRequest get = HttpUtil.createGet(url);
         String body = get.execute().body();
@@ -63,23 +63,84 @@ public class CommonTest {
     }
 
     /*
+    获取店铺列表
+        POST /api/shop/pageList.json
+        系统
+        请求参数
+        Query 参数
+        access_token string 必需 通过获取token接口获得的token，详见 获取 Access Token 默认值: {{access_token}}
+        client_id string 必需 client_id, 获取方式详见 申请API权限 默认值: {{client_id}}
+        timestamp string 必需 13位毫秒时间戳，与当前时间差异不超过正负15分钟，示例：1668153
+        nonce string 必需 随机整数值，保证每个请求唯一，示例：11251 默认值: 121212
+        sign string 必需
+        请求签名，详见 生成sign（签名） 默认值: 121212121
+        Header 参数
+        Content-Type string 可选 固定再header位置加入Content-Type:application/json 默认值: application/json
+        Body 参数 application/json
+        pageNo string 第几页 可选
+        pageSize string 每页大小 可选 示例
+     */
+    @Test
+    public void testGetShopList() throws Exception {
+        //url
+        String url = "https://openapi.sellfox.com/api/shop/pageList.json";
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String nonce = String.valueOf(new Random().nextInt(100000));
+        String sign = generateSign("/api/shop/pageList.json", "post", ACCESS_TOKEN, CLIENT_ID, timestamp, nonce, CLIENT_SECRET);
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("access_token", ACCESS_TOKEN);
+        queryParams.put("client_id", CLIENT_ID);
+        queryParams.put("nonce", nonce);
+        queryParams.put("timestamp", timestamp);
+        queryParams.put("sign", sign);
+        // 拼接URL（处理编码）
+        String fullUrl = HttpUtil.urlWithForm(url, queryParams, null, true);
+
+        Map<String, Object> bodyParams = new HashMap<>();
+//        bodyParams.put("shopId", "");
+//        bodyParams.put("pageNo", "1");
+//        bodyParams.put("pageSize", "10");
+        String body = JSONUtil.toJsonStr(bodyParams);
+
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        HttpResponse response = HttpRequest.post(fullUrl)
+                .headerMap(headers, true)
+                .body(body)
+                .timeout(5000)
+                .execute();
+        //打印完整请求
+        String command = generateCurlCommand(url, queryParams, headers, body);
+        System.out.println(command);
+        if (response.isOk()) {
+            String responseBody = response.body();
+            System.out.println(responseBody);
+        }else {
+            System.out.println("请求失败，状态码：" + response.getStatus());
+            System.out.println("请求失败，响应体：" + response.body());
+        }
+
+    }
+
+    /*
     # 订单详情
      /api/order/detailByOrderId.json**
      请求参数
-    Query 参数
-    access_token string 必需
-    通过获取token接口获得的token，详见 [获取 Access Token](https://sellfoxapi.apifox.cn/doc-1589130)
-    默认值: {{access_token}} client_id string  必需
-    client_id, 获取方式详见 [申请API权限](https://sellfoxapi.apifox.cn/1748360) 默认值: {{client_id}}
-    timestamp string  必需 13位毫秒时间戳，与当前时间差异不超过正负15分钟，示例：1668153260508 默认值: 121212
-    nonce string 必需 随机整数值，保证每个请求唯一，示例：11251 默认值: 121212
-    sign string  必需 请求签名，详见 [生成sign（签名）](https://sellfoxapi.apifox.cn/doc-1749562) 默认值: 121212121
-    Header 参数
-    Content-Type string  可选 固定再header位置加入Content-Type:application/json 默认值: application/json
-    Body 参数application/json
-    shopId string  店铺ID 必需
-    amazonOrderId string 可选 亚马逊订单ID，amazonOrderId，sellerOrderId至少一个不能为空，二者都填则按amazonOrderId搜索
-    sellerOrderId string 可选 卖家订单ID，amazonOrderId，sellerOrderId至少一个不能为空，二者都填则按amazonOrderId搜索
+        Query 参数
+        access_token string 必需
+        通过获取token接口获得的token，详见 [获取 Access Token](https://sellfoxapi.apifox.cn/doc-1589130)
+        默认值: {{access_token}} client_id string  必需
+        client_id, 获取方式详见 [申请API权限](https://sellfoxapi.apifox.cn/1748360) 默认值: {{client_id}}
+        timestamp string  必需 13位毫秒时间戳，与当前时间差异不超过正负15分钟，示例：1668153260508 默认值: 121212
+        nonce string 必需 随机整数值，保证每个请求唯一，示例：11251 默认值: 121212
+        sign string  必需 请求签名，详见 [生成sign（签名）](https://sellfoxapi.apifox.cn/doc-1749562) 默认值: 121212121
+        Header 参数
+        Content-Type string  可选 固定再header位置加入Content-Type:application/json 默认值: application/json
+        Body 参数application/json
+        shopId string  店铺ID 必需
+        amazonOrderId string 可选 亚马逊订单ID，amazonOrderId，sellerOrderId至少一个不能为空，二者都填则按amazonOrderId搜索
+        sellerOrderId string 可选 卖家订单ID，amazonOrderId，sellerOrderId至少一个不能为空，二者都填则按amazonOrderId搜索
      */
     @Test
     public void testGetOrderDetail() throws Exception {
